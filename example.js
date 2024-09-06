651be150cbf6f8ca7ed4f31ec46ccb3560ef2ff7
mhackfly <mhackfly@orange.fr>
2024-09-06 11:34:47 +0200

    add example.js index.html sketch.html

---
+++ example.js
@@ -0,0 +1,22 @@
+function setup() {  
+      
+    createCanvas(400, 300);  
+    iframeBackgroundColor('#0F1121');
+    iframeBoxShadow('inset 15px 15px 15px rgba(0,0,0,.6)');
+    iframeBorder('2px solid #22244B');
+    iframeBorderRadius('10px');
+    iframePadding('10px');
+}  
+  
+function draw() {  
+      
+    strokeWeight(20);   
+    stroke('green');   
+    point(50, 50);     
+    point(50, 250);     
+    point(350, 50);     
+    point(350, 250);     
+    point(200, 150); 
+    
+} 
+
